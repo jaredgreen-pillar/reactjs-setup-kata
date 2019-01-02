8b0a4695e5e8eef1e78cffa232f5233c5653e71c
jaredgreen <jgreen2@pillartechnology.com>
2019-01-02 16:45:37 -0600

    Add textarea and submit button to main page

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('has a button', () => {
+  const appComponent = mount(<App />);
+
+  const button = appComponent.find('button');
+
+  expect(button.text()).toEqual('Submit');
+});
+
+it('has a button', () => {
+  const appComponent = mount(<App />);
+  const textarea = appComponent.find('textarea');
+  expect(textarea.text()).toEqual("");
 });
